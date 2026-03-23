82f93946329abe298503c65f535c4ba9a6301ef1
яна <imaksimmar@gmail.com>
2026-03-23 15:01:00 +0300

    Initial Commit

---
+++ lab2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lab2
+//
+//  Created by yana on 23.03.2026.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
